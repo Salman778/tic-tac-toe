e34e6a74df2531eba7cd024d7ebd5097b5c65612
Salman778 <alk-333@hotmail.com>
2019-05-08 12:42:45 +0300

    add localstorage and enhance the code

--- js/board.js
+++ js/board.js
@@ -1,32 +1,46 @@
  let arr = [ [], [], [] ];
  let count = -1;
- let turn = true;
-
+ let turn;
+ let temp = false
+ let audio;
+ localStorage.setItem("x", 0);
+ localStorage.setItem("o", 0)
  $(document).ready(function () {
    board();
  });
  
  const board = function(){
     $( "body" ).empty();
-    $("body").append(" <ul></ul> <ul></ul> <ul></ul> <button>Restart</button> ");
+    $("body").append(" <div> <ul></ul> <ul></ul> <ul></ul></div> <button>Restart</button> <p id='one'></p> <p id='two'></p>");
     $("ul").append(" <li></li> <li></li> <li></li> ");
-
+    $("#one").text(`X: ${localStorage.getItem('x')}`)
+    $("#two").text(`O: ${localStorage.getItem('o')}`)
     arr = [ [], [], [] ];
-    turn = true;
+    temp = !temp
+    turn = temp;
+    $("li").mouseover(function () { 
+      $(this).css("background", "rgba(33, 62, 74, .5)")
+    }
+    );
 
-     $("li").click(function(){
-       count++;
-       $(this).off("click")
-        if(turn)
-            $(this).text("X")
-        else
-            $(this).text("O")
-            
-        turn = !turn
-        arr[$(this).parents().index()][$(this).index()] = $(this).text();
-        if(count >= 4)
-          setTimeout(check, 100);
-    })
+    $("li").mouseleave(function () { 
+      $(this).css("background", "rgb(33, 62, 74)")
+    });
+
+    $("li").click(function(){
+      count++;
+      $(this).off("click")
+      $(this).off("mouseover")
+      if(turn)
+          $(this).text("X"), audio = new Audio('../audio/Tick.mp3');
+      else
+          $(this).text("O"), audio = new Audio('../audio/Woosh.mp3');
+      audio.play();
+      turn = !turn
+      arr[$(this).parents().index()][$(this).index()] = $(this).text();
+      if(count >= 4)
+        setTimeout(check, 100);
+  })
 
     $("button").click(function(){
       return restart();
@@ -54,15 +68,26 @@
 
 
 
- const restart = function(check=false){
+ const restart = function(check=false){  
    count = -1;
    if(check){
-    if(!turn)
-      alert("X is Win");
-    else
-      alert("O is Win");
-   }
+    audio = new Audio('../audio/applause.mp3');
+    audio.play();
+    if(!turn){
+      setTimeout(function(){
+        alert("X is Win")
+      }, 100);
+      localStorage.setItem("x", (parseInt(localStorage.getItem("x")) + 1))
+      audio = new Audio('../audio/applause.mp3');
+    }
+    else{
+      setTimeout(function(){
+        alert("O is Win")
+      }, 100);
+      localStorage.setItem("o", (parseInt(localStorage.getItem("o")) + 1))
+    }
+  }
   else  
-      alert("Draw");
+      o = 0, x = 0, localStorage.setItem("x", 0), localStorage.setItem("o", 0);
   return board();
  }
